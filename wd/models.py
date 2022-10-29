623fc1e717d70c9091ffc7e4f2b808029fd6a45f
Sgarra <sgarraclaudia0@gmai.com>
2022-10-29 16:59:19 +0200

    First implementation of global attention

--- wd/models.py
+++ wd/models.py
@@ -7,8 +7,191 @@ from torch.nn import functional as F
 from ezdl.utils.utilities import filter_none
 from ezdl.models.backbones.mit import MiTFusion
 from ezdl.models.base import BaseModel
-from ezdl.models.heads.lawin import LawinHead
-from ezdl.models.heads.laweed import LaweedHead
+
+
+from torch import nn, Tensor
+from einops import rearrange
+
+
+RATIOS = [8, 4, 2]
+
+
+class MLP(nn.Module):
+    def __init__(self, dim=2048, embed_dim=768):
+        super().__init__()
+        self.proj = nn.Linear(dim, embed_dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x.flatten(2).transpose(1, 2)
+        x = self.proj(x)
+        return x
+
+
+class PatchEmbed(nn.Module):
+    def __init__(self, patch_size=4, in_ch=3, dim=96, type='pool') -> None:
+        super().__init__()
+        self.patch_size = patch_size
+        self.type = type
+        self.dim = dim
+        
+        if type == 'conv':
+            self.proj = nn.Conv2d(in_ch, dim, patch_size, patch_size, groups=patch_size*patch_size)
+        else:
+            self.proj = nn.ModuleList([
+                nn.MaxPool2d(patch_size, patch_size),
+                nn.AvgPool2d(patch_size, patch_size)
+            ])
+        
+        self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        _, _, H, W = x.shape
+        if W % self.patch_size != 0:
+            x = F.pad(x, (0, self.patch_size - W % self.patch_size))
+        if H % self.patch_size != 0:
+            x = F.pad(x, (0, 0, 0, self.patch_size - H % self.patch_size))
+
+        if self.type == 'conv':
+            x = self.proj(x)
+        else:
+            x = 0.5 * (self.proj[0](x) + self.proj[1](x))
+        Wh, Ww = x.size(2), x.size(3)
+        x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        x = x.transpose(1, 2).view(-1, self.dim, Wh, Ww)
+        return x
+
+
+class ConvModule(nn.Module):
+    def __init__(self, c1, c2):
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, 1, bias=False)
+        self.bn = nn.BatchNorm2d(c2)        # use SyncBN in original
+        self.activate = nn.ReLU(True)
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.activate(self.bn(self.conv(x)))
+
+
+
+class GlobalAttention(nn.Module):
+    def __init__(self, dim, num_heads=8, qkv_bias=False, qk_scale=None, attn_drop=0., proj_drop=0., sr_ratio=1):
+        super().__init__()
+        assert dim % num_heads == 0, f"dim {dim} should be divided by num_heads {num_heads}."
+
+        self.dim = dim
+        self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = qk_scale or head_dim ** -0.5
+
+        self.q = nn.Linear(dim, dim, bias=qkv_bias)
+        self.kv = nn.Linear(dim, dim * 2, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_drop)
+
+        self.sr_ratio = sr_ratio
+        if sr_ratio > 1:
+            self.sr = nn.Conv2d(dim, dim, kernel_size=sr_ratio, stride=sr_ratio)
+            self.norm = nn.LayerNorm(dim)
+
+    def forward(self, x, H, W):
+        B, N, C = x.shape
+        q = self.q(x).reshape(B, N, self.num_heads, C // self.num_heads).permute(0, 2, 1, 3)
+
+        if self.sr_ratio > 1:
+            x_ = x.permute(0, 2, 1).reshape(B, C, H, W)
+            x_ = self.sr(x_).reshape(B, C, -1).permute(0, 2, 1)
+            x_ = self.norm(x_)
+            kv = self.kv(x_).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        else:
+            kv = self.kv(x).reshape(B, -1, 2, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
+        k, v = kv[0], kv[1]
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+        attn = attn.softmax(dim=-1)
+        attn = self.attn_drop(attn)
+
+        x = (attn @ v).transpose(1, 2).reshape(B, N, C)
+        x = self.proj(x)
+        x = self.proj_drop(x)
+
+        return x
+
+
+class NewLawinHead(nn.Module):
+    def __init__(self, in_channels: list, embed_dim=512, num_classes=19) -> None:
+        super().__init__()
+        for i, dim in enumerate(in_channels):
+            self.add_module(f"linear_c{i+1}", MLP(dim, 48 if i == 0 else embed_dim))
+
+        self.ratios = RATIOS
+        self.lawin_8 = GlobalAttention(embed_dim, 64)
+        self.lawin_4 = GlobalAttention(embed_dim, 16)
+        self.lawin_2 = GlobalAttention(embed_dim, 4)
+        self.ds_8 = PatchEmbed(8, embed_dim, embed_dim)
+        self.ds_4 = PatchEmbed(4, embed_dim, embed_dim)
+        self.ds_2 = PatchEmbed(2, embed_dim, embed_dim)
+    
+        self.image_pool = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            ConvModule(embed_dim, embed_dim)
+        )
+        self.linear_fuse = ConvModule(embed_dim*3, embed_dim)
+        self.short_path = ConvModule(embed_dim, embed_dim)
+        self.cat = ConvModule(embed_dim*5, embed_dim)
+
+        self.low_level_fuse = ConvModule(embed_dim+48, embed_dim)
+        self.linear_pred = nn.Conv2d(embed_dim, num_classes, 1)
+        self.dropout = nn.Dropout2d(0.1)
+    
+    def get_lawin_att_feats(self, x: Tensor, patch_size: int, ratios: list, step: str = "") -> list:
+        _, _, H, W = x.shape
+        query = F.unfold(x, patch_size, stride=patch_size)
+        query = rearrange(query, 'b (c ph pw) (nh nw) -> (b nh nw) c ph pw', ph=patch_size, pw=patch_size, nh=H//patch_size, nw=W//patch_size)
+        outs = []
+
+        for r in ratios:
+            context = F.unfold(x, patch_size*r, stride=patch_size, padding=int((r-1)/2*patch_size))
+            context = rearrange(context, "b (c ph pw) (nh nw) -> (b nh nw) c ph pw", ph=patch_size*r, pw=patch_size*r, nh=H//patch_size, nw=W//patch_size)
+            context = getattr(self, f"ds_{step}{r}")(context)
+            output = getattr(self, f"lawin_{step}{r}")(query, context)
+            output = rearrange(output, "(b nh nw) c ph pw -> b c (nh ph) (nw pw)", ph=patch_size, pw=patch_size, nh=H//patch_size, nw=W//patch_size)
+            outs.append(output)
+        return outs
+
+    def forward(self, features):
+        B, _, H, W = features[1].shape
+        outs = [self.linear_c2(features[1]).permute(0, 2, 1).reshape(B, -1, *features[1].shape[-2:])]
+
+        for i, feature in enumerate(features[2:]):
+            cf = eval(f"self.linear_c{i+3}")(feature).permute(0, 2, 1).reshape(B, -1, *feature.shape[-2:])
+            outs.append(F.interpolate(cf, size=(H, W), mode='bilinear', align_corners=False))
+
+        feat = self.linear_fuse(torch.cat(outs[::-1], dim=1))
+        B, _, H, W = feat.shape
+
+        ## Lawin attention spatial pyramid pooling
+        feat_short = self.short_path(feat)
+        feat_pool = F.interpolate(self.image_pool(feat), size=(H, W), mode='bilinear', align_corners=False)
+        feat_lawin = self.get_lawin_att_feats(feat, 8, self.ratios)
+        output = self.cat(torch.cat([feat_short, feat_pool, *feat_lawin], dim=1))
+
+        ## Low-level feature enhancement
+        c1 = self.linear_c1(features[0]).permute(0, 2, 1).reshape(B, -1, *features[0].shape[-2:])
+        output = F.interpolate(output, size=features[0].shape[-2:], mode='bilinear', align_corners=False)
+        fused = self.low_level_fuse(torch.cat([output, c1], dim=1))
+
+        seg = self.linear_pred(self.dropout(fused))
+        return seg
+
+
+
+
+
+
+
+
 
 
 class BaseLawin(BaseModel):
@@ -47,17 +230,17 @@ class Lawin(BaseLawin):
     """
 
     def __init__(self, arch_params) -> None:
-        super().__init__(arch_params, LawinHead)
+        super().__init__(arch_params, NewLawinHead)
 
 
-class Laweed(BaseLawin):
-    """
+"""class Laweed(BaseLawin):
+    
     Notes::::: This implementation has larger params and FLOPs than the results reported in the paper.
     Will update the code and weights if the original author releases the full code.
-    """
+    
 
     def __init__(self, arch_params) -> None:
-        super().__init__(arch_params, LaweedHead)
+        super().__init__(arch_params, LaweedHead)"""
 
 
 class BaseDoubleLawin(BaseLawin):
@@ -98,14 +281,14 @@ class BaseDoubleLawin(BaseLawin):
         return y
 
 
-class DoubleLawin(BaseDoubleLawin):
+"""class DoubleLawin(BaseDoubleLawin):
     def __init__(self, arch_params) -> None:
         super().__init__(arch_params, LawinHead)
 
 
 class DoubleLaweed(BaseDoubleLawin):
     def __init__(self, arch_params) -> None:
-        super().__init__(arch_params, LaweedHead)
+        super().__init__(arch_params, LaweedHead)"""
 
 
 class BaseSplitLawin(BaseLawin):
@@ -139,7 +322,9 @@ class BaseSplitLawin(BaseLawin):
         first_feat_main = self.backbone.partial_forward(main_channels, slice(0, 1))
         first_feat = self.fusion((first_feat_main, first_feat_side))[0]
         feat = (first_feat,) + self.backbone.partial_forward(first_feat, slice(1, 4))
+
         y = self.decode_head(feat)  # 4x reduction in image size
+
         y = F.interpolate(y, size=x.shape[2:], mode='bilinear', align_corners=False)  # to original image shape
         return y
 
@@ -160,18 +345,18 @@ class BaseSplitLawin(BaseLawin):
 
 class SplitLawin(BaseSplitLawin):
     def __init__(self, arch_params) -> None:
-        super().__init__(arch_params, LawinHead)
+        super().__init__(arch_params, NewLawinHead)
 
 
-class SplitLaweed(BaseSplitLawin):
+"""class SplitLaweed(BaseSplitLawin):
     def __init__(self, arch_params) -> None:
-        super().__init__(arch_params, LaweedHead)
+        super().__init__(arch_params, LaweedHead)"""
 
 
 # Legacy names
 lawin = Lawin
-laweed = Laweed
-doublelawin = DoubleLawin
-doublelaweed = DoubleLaweed
+#laweed = Laweed
+#doublelawin = DoubleLawin
+#doublelaweed = DoubleLaweed
 splitlawin = SplitLawin
-splitlaweed = SplitLaweed
+#splitlaweed = SplitLaweed
